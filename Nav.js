7fab76d5e03f8ef274122d7214d217d7a221ed53
Aurelien Visentin <aurelien.visentin@epita.fr>
2019-03-12 11:59:04 +0100

    Added Downloads and polished the website

---
+++ Nav.js
@@ -0,0 +1,7 @@
+document.write(' \
+<h2>Veuillez sélectionner une page : </h2> <!-- Titre de niveau 2 -->\
+        <ul>														<!-- Liste a puce -->\
+            <li><a href=index.html>Page principale</a></li>			<!-- Puce -->\
+			<li><a href=downloads.html>Téléchargements</a></li>		<!-- Puce -->\
+        </ul>\
+');
