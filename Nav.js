eadae182d1e14a7b666effa5089eca4566685cf9
aurelien.visentin <aurelien.visentin@epita.fr>
2019-04-16 12:30:45 +0200

    Medias and Nav
    Creation of the media page
    Added soutenance 1 videos
    
    Updated nav script

--- Nav.js
+++ Nav.js
@@ -1,7 +1,9 @@
 document.write(' \
-<h2>Veuillez sélectionner une page : </h2> <!-- Titre de niveau 2 -->\
-        <ul>														<!-- Liste a puce -->\
+<h3>Veuillez sélectionner une page : </h2> <!-- Titre de niveau 2 -->\
+        <ul>														<!-- Liste à puce -->\
             <li><a href=index.html>Page principale</a></li>			<!-- Puce -->\
-			<li><a href=downloads.html>Téléchargements</a></li>		<!-- Puce -->\
+            <li><a href=about.html>À propos</a></li>		        <!-- Puce -->\
+            <li><a href=medias.html>Médias</a></li>		            <!-- Puce -->\
+            <li><a href=downloads.html>Téléchargements</a></li>		<!-- Puce -->\
         </ul>\
 ');
